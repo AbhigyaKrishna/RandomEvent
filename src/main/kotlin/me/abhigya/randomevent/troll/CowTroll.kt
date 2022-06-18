2765b8d0f677c1624429afbb9f3384a990ca6c3b
royraj <royraj.947493@gmail.com>
2022-06-19 00:31:07 +0530

    Added NMS and Tried to Add the De-Officiator.

--- src/main/kotlin/me/abhigya/randomevent/troll/CowTroll.kt
+++ src/main/kotlin/me/abhigya/randomevent/troll/CowTroll.kt
@@ -8,10 +8,14 @@ class CowTroll : RandomTrollList<PlayerInteractAtEntityEvent>() {
     private val turnCowToChicken = Troll<PlayerInteractAtEntityEvent> ("Turn Cow To Chicken", 50) {
         val location = it.rightClicked.location
         it.rightClicked.remove()
-        it.rightClicked.world.spawnEntity(location, EntityType.CHICKEN)
+        val chicken = it.rightClicked.world.spawnEntity(location, EntityType.CHICKEN)
+    }
+
+    private val DoNothing = Troll<PlayerInteractAtEntityEvent> ("Do Nothing", 50) {
+        it.isCancelled = false
     }
 
     override fun list(): List<Troll<PlayerInteractAtEntityEvent>> {
-        return listOf(turnCowToChicken)
+        return listOf(turnCowToChicken, DoNothing)
     }
 }
